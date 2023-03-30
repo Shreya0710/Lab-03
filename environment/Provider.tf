565340763ead4defaa32db1d33efec8b761f2e00
Shreya0710 <78745673+Shreya0710@users.noreply.github.com>
2023-03-30 17:21:07 -0400

    Update Provider.tf

--- environment/Provider.tf
+++ environment/Provider.tf
@@ -1,12 +1,3 @@
-# Configuration of the provider
-terraform {
-required_providers {
-aws = {
-source = "hashicorp/aws"
-   }
-  }
-}
-
 # Configuration of the AWS provider parameters
 provider "aws" {
 region = "us-east-1"
